10990cb40fd4cb64f7e28955af101902279d0637
JakeW <jakewellsd@gmail.com>
2021-01-07 20:18:28 +0000

    Create Release Model

---
+++ MusicRater/Models/Release.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models
+{
+    public class Release
+    {
+        public long ReleaseID { get; set; }
+        public string Title { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public  ReleaseType ReleaseType { get; set; }
+    }
+
+    public enum ReleaseType
+    {
+        Album,
+        Compilation,
+        Ep,
+        Mixtape,
+        Single,
+        Live,
+        DjMix,
+        Bootleg,
+    }
+}
