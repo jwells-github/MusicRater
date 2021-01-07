ff317321f2720baee2e32f6ff90d414805cf604e
JakeW <jakewellsd@gmail.com>
2021-01-07 16:05:08 +0000

    Add Identity Scaffolding

---
+++ MusicRater/Areas/Identity/Data/MusicRaterUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MusicRater.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MusicRaterUser class
+    public class MusicRaterUser : IdentityUser
+    {
+    }
+}
---
+++ MusicRater/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MusicRater.Areas.Identity.Data;
+using MusicRater.Data;
+
+[assembly: HostingStartup(typeof(MusicRater.Areas.Identity.IdentityHostingStartup))]
+namespace MusicRater.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MusicRaterContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MusicRaterContextConnection")));
+
+                services.AddDefaultIdentity<MusicRaterUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MusicRaterContext>();
+            });
+        }
+    }
+}
