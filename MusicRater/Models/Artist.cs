baca26c7398c28f46e15dde1098157997c6e8821
JakeW <jakewellsd@gmail.com>
2021-01-07 20:18:21 +0000

    Create Artist Model

---
+++ MusicRater/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models
+{
+    public class Artist
+    {
+        public long ArtistID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Release> Releases { get; set; }
+
+    }
+}
