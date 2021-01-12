6309397f731f93593a971b24904f7b3d00f8f404
JakeW <jakewellsd@gmail.com>
2021-01-12 20:53:58 +0000

    Create ReleaseRating model

---
+++ MusicRater/Models/ReleaseRating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicRater.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicRater.Models
+{
+    public class ReleaseRating
+    {
+        public long ReleaseRatingID { get; set; }
+        [Range(1,10)]
+        public int Rating { get; set; }
+        public DateTime RatingDate { get; set; }
+        public string UserID { get; set; }
+        public MusicRaterUser User { get; set; }
+        public Release Release { get; set; }
+
+    }
+}
