d5820b4872104227a1b44db8bd5ea06dd0d1f94c
JakeW <jakewellsd@gmail.com>
2021-01-12 22:39:21 +0000

    Display list of user ratings on home page

--- MusicRater/Controllers/HomeController.cs
+++ MusicRater/Controllers/HomeController.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MusicRater.Models;
+using MusicRater.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using MusicRater.Data;
 
 namespace MusicRater.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private UserManager<MusicRaterUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<MusicRaterUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
-            return View();
+            MusicRaterUser user = await _userManager.GetUserAsync(User);
+            MusicRaterUser populatedUser = await _userManager.Users.Include(u => u.ReleaseRatings).FirstOrDefaultAsync(u => u.Id == user.Id); ;
+            return View(populatedUser);
         }
 
         public IActionResult Privacy()
         {
+
             return View();
         }
 
--- MusicRater/Controllers/ReleaseController.cs
+++ MusicRater/Controllers/ReleaseController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-
+using MusicRater.Areas.Identity.Data;
+using MusicRater.Data;
 using MusicRater.Models;
 
 namespace MusicRater.Controllers
@@ -14,12 +16,16 @@ namespace MusicRater.Controllers
     public class ReleaseController : Controller
     {
         private readonly ILogger<ReleaseController> _logger;
-        private MusicRaterDbContext context;
+        private MusicRaterContext context;
+        private UserManager<MusicRaterUser> _userManager;
 
-        public ReleaseController(ILogger<ReleaseController> logger, MusicRaterDbContext data)
+        public ReleaseController(ILogger<ReleaseController> logger,
+            MusicRaterContext data,
+            UserManager<MusicRaterUser> userManager)
         {
             context = data;
             _logger = logger;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -37,9 +43,6 @@ namespace MusicRater.Controllers
         [HttpPost]
         public async Task<IActionResult> New(long artistID, [FromForm] Release release)
         {
-            Debug.WriteLine("???");
-            Debug.WriteLine(Json(release));
-            Debug.WriteLine(release.Title);
             Artist artist = await context.Artists.FirstOrDefaultAsync(a => a.ArtistID == artistID);
             release.Artist = artist;
             if (ModelState.IsValid)
@@ -55,6 +58,21 @@ namespace MusicRater.Controllers
         {
             Release release = await context.Releases.FirstOrDefaultAsync(r => r.ReleaseID == id);
             return View(release);
+        }
+
+        public async Task <IActionResult> Rate(long id, [FromForm] int rating)
+        {
+            Release release = await context.Releases.FirstOrDefaultAsync(r => r.ReleaseID == id);
+            MusicRaterUser user = await _userManager.GetUserAsync(User);
+            ReleaseRating releaseRating = new ReleaseRating();
+            releaseRating.Rating = rating;
+            releaseRating.RatingDate = DateTime.Now;
+            releaseRating.UserID = user.Id;
+            releaseRating.User = user;
+            releaseRating.Release = release;
+            context.ReleaseRating.Add(releaseRating);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Entry), new { id });
         }
     }
 }
