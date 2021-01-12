e9a4eef999c22c445a56277f2609ad9dd234e2af
JakeW <jakewellsd@gmail.com>
2021-01-12 20:53:24 +0000

    Create empty release collection in constructor

--- MusicRater/Models/Artist.cs
+++ MusicRater/Models/Artist.cs
@@ -7,6 +7,10 @@ namespace MusicRater.Models
 {
     public class Artist
     {
+        public Artist()
+        {
+            this.Releases = new HashSet<Release>();
+        }
         public long ArtistID { get; set; }
         public string Name { get; set; }
         public ICollection<Release> Releases { get; set; }
