1b3b228fbe4efa84f43ba85c5be691f41c41883d
JakeW <jakewellsd@gmail.com>
2021-01-12 16:58:43 +0000

    Add collection of ReleaseRatings to user

--- MusicRater/Areas/Identity/Data/MusicRaterUser.cs
+++ MusicRater/Areas/Identity/Data/MusicRaterUser.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using MusicRater.Models;
+
 
 namespace MusicRater.Areas.Identity.Data
 {
     // Add profile data for application users by adding properties to the MusicRaterUser class
     public class MusicRaterUser : IdentityUser
     {
+
+        public MusicRaterUser()
+        {
+            this.ReleaseRatings = new HashSet<ReleaseRating>();
+        }
+        public ICollection<ReleaseRating> ReleaseRatings { get; set; }
     }
 }
