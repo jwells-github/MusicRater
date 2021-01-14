7748845e623263032dc566d5b17e86bdbef5be19
JakeW <jakewellsd@gmail.com>
2021-01-14 14:45:52 +0000

    Mark artist name as a required field

--- MusicRater/Models/Artist.cs
+++ MusicRater/Models/Artist.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicRater.Models
 {
@@ -12,6 +13,7 @@ namespace MusicRater.Models
             this.Releases = new HashSet<Release>();
         }
         public long ArtistID { get; set; }
+        [Required]
         public string Name { get; set; }
         public ICollection<Release> Releases { get; set; }
 
