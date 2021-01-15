671d817d2dbd51a3df0dbfb9daebe4e9a2cc5535
JakeW <jakewellsd@gmail.com>
2021-01-15 14:41:43 +0000

    Display match results for artists, releases and users for default search

---
+++ MusicRater/Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MusicRater.Models;
+using MusicRater.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using MusicRater.Data;
+
+namespace MusicRater.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+        private UserManager<MusicRaterUser> _userManager;
+        private MusicRaterContext context;
+
+        public SearchController(ILogger<SearchController> logger,
+            UserManager<MusicRaterUser> userManager,
+            MusicRaterContext data)
+        {
+            context = data;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public async Task <IActionResult> Index(string searchTerm)
+        {
+            var artists = await context.Artists.Where(a => a.Name.Contains(searchTerm)).ToListAsync();
+            var releases = await context.Releases.Where(r => r.Title.Contains(searchTerm)).ToListAsync();
+            var users = await context.Users.Where(u => u.UserName.Contains(searchTerm)).ToListAsync();
+            SearchViewModel searchViewModel = new SearchViewModel
+            {
+                Artists =  artists,
+                Releases =  releases,
+                Users =  users
+            };
+            return View("searchResults", searchViewModel);
+        }
+    }
+}
---
+++ MusicRater/Models/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicRater.Areas.Identity.Data;
+namespace MusicRater.Models
+{
+    public class SearchViewModel
+    {
+        public List<Artist> Artists { get; set; }
+        public List<Release> Releases { get; set; }
+        public List<MusicRaterUser> Users { get; set; }
+    }
+}
