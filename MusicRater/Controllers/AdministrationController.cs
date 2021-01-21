89be4bc2b33380cb48780f72813ab060697799d7
JakeW <jakewellsd@gmail.com>
2021-01-21 16:27:24 +0000

    Add route to enable to user account as an admin

---
+++ MusicRater/Controllers/AdministrationController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MusicRater.Areas.Identity.Data;
+using MusicRater.Data;
+using MusicRater.Models;
+
+namespace MusicRater.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private  IConfiguration _config;
+        private UserManager<MusicRaterUser> _userManager;
+        public AdministrationController(UserManager<MusicRaterUser> userManager, IConfiguration config)
+        {
+            _config = config;
+            _userManager = userManager;
+        }
+        
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult New()
+        {
+            return View();
+        }
+        
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> New(string password)
+        {
+            var adminPassword = _config["AdminPassword"];
+            if (password == adminPassword)
+            {
+                MusicRaterUser user = await _userManager.GetUserAsync(User);
+                await _userManager.AddToRoleAsync(user, "Administrator");
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+
+        }
+    }
+}
