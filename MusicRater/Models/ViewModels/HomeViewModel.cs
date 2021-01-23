e4bee2aa2eee82028d330958b794f2009dbde5b8
JakeW <jakewellsd@gmail.com>
2021-01-23 20:54:53 +0000

    Create HomeViewModel

---
+++ MusicRater/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MusicRater.Areas.Identity.Data;
+
+namespace MusicRater.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public MusicRaterUser user { get; set; }
+        public ICollection<Release> recentReleases { get; set; }
+
+    }
+}
