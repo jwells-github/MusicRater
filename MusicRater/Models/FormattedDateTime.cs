3eda14b9276bb96b0cda23b1b4dc35443341bd02
JakeW <jakewellsd@gmail.com>
2021-01-23 20:54:39 +0000

    Create Method to return a DateTime from three date integers

---
+++ MusicRater/Models/FormattedDateTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models
+{
+    public class FormattedDateTime
+    {
+        public static DateTime GetFormattedDate(int releaseDay, int releaseMonth, int releaseYear)
+        {
+            if (releaseDay == 0 && releaseMonth == 0 && releaseYear == 0)
+            {
+                return DateTime.MaxValue;
+            }
+            return new DateTime(releaseYear, releaseMonth, releaseDay);
+        }
+    }
+}
