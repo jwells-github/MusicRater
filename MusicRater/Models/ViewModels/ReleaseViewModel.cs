5818e5f2bd6d6e58e45057fb3b5df27d8a4bfc3c
JakeW <jakewellsd@gmail.com>
2021-01-25 15:09:02 +0000

    Move MonthString method to DateSpanPartial

--- MusicRater/Models/ViewModels/ReleaseViewModel.cs
+++ MusicRater/Models/ViewModels/ReleaseViewModel.cs
@@ -45,38 +45,5 @@ namespace MusicRater.Models
         public ICollection<ReleaseGenre> ReleaseGenres { get; set; }
 
         public List<SelectListItem> Months { get; } = MonthSelectList.GetMonthList();
-
-        public String MonthString()
-        {
-            switch (this.ReleaseMonth)
-            {
-                case 1:
-                    return "January";
-                case 2:
-                    return "February";
-                case 3:
-                    return "March";
-                case 4:
-                    return "April";
-                case 5:
-                    return "May";
-                case 6:
-                    return "June";
-                case 7:
-                    return "July";
-                case 8:
-                    return "August";
-                case 9:
-                    return "September";
-                case 10:
-                    return "October";
-                case 11:
-                    return "November";
-                case 12:
-                    return "December";
-                default:
-                    return "";
-            }
-        }
     }
 }
