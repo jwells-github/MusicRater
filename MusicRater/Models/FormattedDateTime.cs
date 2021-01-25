20520b4cc4da1092b1f1a03df7603a922b835317
JakeW <jakewellsd@gmail.com>
2021-01-25 21:03:44 +0000

    Change variable names to be more generic

--- MusicRater/Models/FormattedDateTime.cs
+++ MusicRater/Models/FormattedDateTime.cs
@@ -7,16 +7,16 @@ namespace MusicRater.Models
 {
     public class FormattedDateTime
     {
-        public static DateTime GetFormattedDate(int releaseDay, int releaseMonth, int releaseYear)
+        public static DateTime GetFormattedDate(int day, int month, int year)
         {
-            if (releaseDay == 0 && releaseMonth == 0 && releaseYear == 0)
+            if (day == 0 && month == 0 && year == 0)
             {
                 return DateTime.MaxValue;
             }
-            releaseDay = releaseDay == 0 ? 1 : releaseDay;
-            releaseMonth = releaseMonth == 0 ? 1 : releaseMonth;
-            releaseYear = releaseYear == 0 ? 1 : releaseYear;
-            return new DateTime(releaseYear, releaseMonth, releaseDay);
+            day = day == 0 ? 1 : day;
+            month = month == 0 ? 1 : month;
+            year = year == 0 ? 1 : year;
+            return new DateTime(year, month, day);
         }
     }
 }
