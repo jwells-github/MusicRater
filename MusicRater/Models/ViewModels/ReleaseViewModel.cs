42d90bdf3b2432a66991fea0a6c920ea61806987
JakeW <jakewellsd@gmail.com>
2021-01-25 14:17:51 +0000

    GetMonthList from MonthSelectList

--- MusicRater/Models/ViewModels/ReleaseViewModel.cs
+++ MusicRater/Models/ViewModels/ReleaseViewModel.cs
@@ -44,22 +44,8 @@ namespace MusicRater.Models
         
         public ICollection<ReleaseGenre> ReleaseGenres { get; set; }
 
-        public List<SelectListItem> Months { get; } = new List<SelectListItem>
-        {
-            new SelectListItem  { Value = "1", Text = "January" },
-            new SelectListItem  { Value = "2", Text = "February" },
-            new SelectListItem  { Value = "3", Text = "March" },
-            new SelectListItem  { Value = "4", Text = "April" },
-            new SelectListItem  { Value = "5", Text = "May" },
-            new SelectListItem  { Value = "6", Text = "June" },
-            new SelectListItem  { Value = "7", Text = "July" },
-            new SelectListItem  { Value = "8", Text = "August" },
-            new SelectListItem  { Value = "9", Text = "September" },
-            new SelectListItem  { Value = "10", Text = "October" },
-            new SelectListItem  { Value = "11", Text = "November" },
-            new SelectListItem  { Value = "12", Text = "December" },
-        };
-        
+        public List<SelectListItem> Months { get; } = MonthSelectList.GetMonthList();
+
         public String MonthString()
         {
             switch (this.ReleaseMonth)
