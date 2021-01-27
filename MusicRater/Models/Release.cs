2682424f50006ad189e60105cd5f1d080362e4b7
JakeW <jakewellsd@gmail.com>
2021-01-27 21:43:14 +0000

    Set a random RGB value to represent the album cover

--- MusicRater/Models/Release.cs
+++ MusicRater/Models/Release.cs
@@ -11,35 +11,36 @@ namespace MusicRater.Models
         public Release()
         {
             this.UserReleaseRatings = new HashSet<ReleaseRating>();
+            Random random = new Random();
+            this.RGBAlbumCoverBlue = random.Next(256);
+            this.RGBAlbumCoverRed = random.Next(256);
+            this.RGBAlbumCoverGreen = random.Next(256);
         }
-
         public long ReleaseID { get; set; }
         [Required]
         public string Title { get; set; }
-
         [Range(0,31)]
         public int ReleaseDay { get; set; }
         [Range(0, 12)]
         public int ReleaseMonth { get; set; }
         public int ReleaseYear { get; set; }
-
         public DateTime FormattedDate { get; set; }
-
         public  ReleaseType Type { get; set; }
-
         public long ArtistID { get; set; }
-
         public Artist Artist { get; set; }
-
         public double AverageRating { get; set; }
         public int NumberOfRatings { get; set; }
         public int NumberOfReviews { get; set; }
         public ICollection<ReleaseReview> releaseReviews { get; set; }
         public ICollection<ReleaseRating> UserReleaseRatings { get; set; }
-
         public ICollection<ReleaseGenre> ReleaseGenres { get; set; }
-
-
+        public int RGBAlbumCoverRed { get; set; }
+        public int RGBAlbumCoverGreen { get; set; }
+        public int RGBAlbumCoverBlue { get; set; }
+        public string GetBackgroundCSS()
+        {
+            return $"background-color: rgb({this.RGBAlbumCoverRed},{this.RGBAlbumCoverGreen},{this.RGBAlbumCoverBlue});";
+        }
     }
 
     public enum ReleaseType
