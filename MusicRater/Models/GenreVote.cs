2b38ca7f3cbda8640fbd89e1a698963988a1db31
JakeW <jakewellsd@gmail.com>
2021-02-02 20:42:18 +0000

    Add GenreVote to ReleaseGenre

---
+++ MusicRater/Models/GenreVote.cs
@@ -0,0 +1,18 @@
+﻿using MusicRater.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MusicRater.Models
+{
+    public class GenreVote
+    {
+        public long GenreVoteID { get; set; }
+        public long ReleaseGenreID { get; set; }
+        public ReleaseGenre ReleaseGenre { get; set; }
+        public string UserID { get; set; }
+        public MusicRaterUser User { get; set; }
+    }
+}
--- MusicRater/Models/ReleaseGenre.cs
+++ MusicRater/Models/ReleaseGenre.cs
@@ -8,17 +8,18 @@ namespace MusicRater.Models
 {
     public class ReleaseGenre
     {
+        public long ReleaseGenreID { get; set; }
         public ReleaseGenre()
         {
-            this.UserVotes = new HashSet<MusicRaterUser>();
+            this.GenreVotes = new HashSet<GenreVote>();
         }
-        public string GenreID { get; set; }
         public int GenreVoting { get; set; }
+        public string GenreID { get; set; }  
         public Genre Genre { get; set; }
         public long ReleaseID { get; set; }
         public Release Release { get; set; }
         public long ArtistID { get; set; }
 
-        public ICollection<MusicRaterUser> UserVotes { get; set; }
+        public ICollection<GenreVote> GenreVotes { get; set; }
     }
 }
