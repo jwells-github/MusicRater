88aa89b85cdf8c525e3fc5cd3ae11b9920d5acd4
JakeW <jakewellsd@gmail.com>
2021-02-15 15:39:14 +0000

    Rename route from index to profile, get user's most recent ratings and most recent reviews

--- MusicRater/Controllers/UserController.cs
+++ MusicRater/Controllers/UserController.cs
@@ -11,6 +11,7 @@ using MusicRater.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using MusicRater.Data;
 using Microsoft.AspNetCore.Authorization;
+using MusicRater.Models.ViewModels;
 
 namespace MusicRater.Controllers
 {
@@ -19,22 +20,57 @@ namespace MusicRater.Controllers
 
         private readonly ILogger<UserController> _logger;
         private UserManager<MusicRaterUser> _userManager;
+        private MusicRaterContext context;
 
-        public UserController(ILogger<UserController> logger, UserManager<MusicRaterUser> userManager)
+        public UserController(ILogger<UserController> logger,
+            UserManager<MusicRaterUser> userManager,
+            MusicRaterContext data)
         {
+            context = data;
             _logger = logger;
             _userManager = userManager;
         }
 
         [AllowAnonymous]
-        public async Task <IActionResult> Index(string username)
+        public async Task<IActionResult> Profile(string id)
         {
-            MusicRaterUser user =
-                await _userManager.Users.Include(user => user.ReleaseRatings)
-                .ThenInclude(rating => rating.Release)
-                .ThenInclude(release => release.Artist)
-                .FirstOrDefaultAsync(u => u.UserName == username);
-            return View(user);
+            // TODO: User not found
+            int displayRatings = 5;
+            int displayReviews = 5;
+            MusicRaterUser currentUser = await _userManager.GetUserAsync(User);
+            MusicRaterUser user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == id);
+            UserProfile userProfile = await context.UserProfiles.FirstOrDefaultAsync(u => u.UserID == user.Id);
+            if(userProfile == null)
+            {
+                userProfile = new UserProfile { UserID = user.Id, User = user };
+                context.UserProfiles.Add(userProfile);
+                context.SaveChanges();
+            }
+            UserProfileViewModel userProfileViewModel = new UserProfileViewModel
+            {
+                User = user,
+                UserProfile = userProfile,
+                IsProfileOwner = (currentUser.Id == user.Id),
+                RecentRatings = await context.ReleaseRating
+                    .Where(r => r.UserID == user.Id)
+                    .OrderBy(r => r.RatingDate)
+                    .Skip(0)
+                    .Take(displayRatings)
+                    .Include(rating => rating.Release)
+                    .ThenInclude(release => release.Artist)
+                    .ToListAsync(),
+                RecentReviews = await context.ReleaseReviews
+                    .Where(r => r.UserID == user.Id)
+                    .OrderBy(r=>r.ReviewDate)
+                    .Skip(0)
+                    .Take(displayReviews)
+                    .Include(r => r.Release)
+                    .ThenInclude(r=> r.Artist)
+                    .ToListAsync(),
+               
+            };
+            return View(userProfileViewModel);
         }
+
     }
 }
