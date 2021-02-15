f69e53abbe483795ea958d29f2cbac63cf99d7d2
JakeW <jakewellsd@gmail.com>
2021-02-15 15:37:55 +0000

    Add UserProfileViewModel

---
+++ MusicRater/Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MusicRater.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public MusicRaterUser User { get; set; }
+        public ICollection<ReleaseRating> RecentRatings { get; set; }
+        public ICollection<ReleaseReview> RecentReviews { get; set; }   
+        public UserProfile UserProfile { get; set; }
+        public bool IsProfileOwner { get; set; }
+    }
+}
