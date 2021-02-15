e428b5b4a9b0acb42f3bbd4dc152c5478ae0873c
JakeW <jakewellsd@gmail.com>
2021-02-15 17:12:46 +0000

    Raplace birth DateTime with three int varaibles

--- MusicRater/Models/UserProfile.cs
+++ MusicRater/Models/UserProfile.cs
@@ -1,6 +1,7 @@
 ﻿using MusicRater.Areas.Identity.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,8 +15,12 @@ namespace MusicRater.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
-        public DateTime BirthDate { get; set; }
+        [Range(0, 31)]
+        public int BirthDay { get; set; }
+        [Range(0, 12)]
+        public int BirthMonth { get; set; }
+        public int BirthYear { get; set; }
         public Country Country { get; set; }
-        public string About { get; set; }
+        public string Biography { get; set; }
     }
 }
