68f6c7e1d8a40fc5cc4541eec933ab4ea800d9c7
JakeW <jakewellsd@gmail.com>
2021-03-01 20:51:59 +0000

    Add Viewmodel for administratior index action

---
+++ MusicRater/Models/ViewModels/AdministrationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRater.Models.ViewModels
+{
+    public class AdministrationViewModel
+    {
+        public ICollection<ArtistEditRequest> RecentArtistRequests { get; set; }
+        public int ArtistRequestCount { get; set; }
+
+        public ICollection<ReleaseEditRequest> RecentReleaseRequests { get; set; }
+        public int ReleaseRequestCount { get; set; }
+    }
+}
