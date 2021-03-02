18f940d4014f228c1b7c333643fdc3bc9e0acb81
JakeW <jakewellsd@gmail.com>
2021-03-02 21:50:21 +0000

    Add method to check if an entered date is acceptable

--- MusicRater/Models/FormattedDateTime.cs
+++ MusicRater/Models/FormattedDateTime.cs
@@ -18,5 +18,22 @@ namespace MusicRater.Models
             year = year == 0 ? 1 : year;
             return new DateTime(year, month, day);
         }
+        public static bool DateIsValid(int day, int month, int year)
+        {
+            // Do not allow a day value if month or year have no value
+            if(day != 0)
+            {
+                if(month == 0 || year == 0)
+                {
+                    return false;
+                }
+            }
+            // Do not allow a month value if year has no value
+            if(month != 0 && year == 0)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
